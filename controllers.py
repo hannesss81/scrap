33df09d25165c93e7b23b41ee5bc114c686351d2
Hannes Metssalu <hannes.metssalu@zeroturnaround.com>
2016-12-11 23:03:42 +0200

    changed project idea, started working on getting data from rss feeds properly

--- controllers.py
+++ controllers.py
@@ -1,27 +1,26 @@
 import cherrypy, os, threading
 
 from mako.template import Template
-from util import connection, htmlparser
+from util import htmlparser
 
 class Index:
     @cherrypy.expose
     def index(self):
-        return """
-        <a href="/scrape/" >Scrape</a>
-        """
+        return "Index page. Nothing to see here :)."
 
 class Scrape:
     @cherrypy.expose
     def index(self):
-        connection.open_connections(["http://www.postimees.ee"])
         return Template(filename=getTemplatePath("templates/footemplate.html")).render()
 
-def getTemplatePath(template_name):
-    return os.path.join(template_name)
 
+# Entry point
 root = Index()          # "/"
 root.scrape = Scrape()  # "/scrape"
 
 if __name__ == '__main__':
-    threading.Thread(target=htmlparser.startParsing, name="HTMLParser").start()
+    threading.Thread(target=htmlparser.startParsing, name="RSSParser").start()
     cherrypy.quickstart(root)
+
+def getTemplatePath(template_name):
+    return os.path.join(template_name)
--- util/htmlparser.py
+++ util/htmlparser.py
@@ -1,21 +1,48 @@
-import time, threading, lxml.html
+import time, threading, feedparser
 
-concurrent_html = []
+rss_feed_queue = ["http://www.ft.com/rss/markets/europe",
+                  "http://feed.businesswire.com/rss/home/?rss=G1QFDERJXkJeEFpRVQ==",
+                  "https://rss.sciencedaily.com/matter_energy/engineering.xml",
+                  "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"]
 
 def startParsing():
-    global concurrent_html
-    print("HTML Parsing initialized.")
+    global rss_feed_queue
+    print("RSS Feed parsing initialized.")
 
     while True:
-        if (concurrent_html):
-            threading.Thread(target=parseHtml, args=[concurrent_html.pop()]).start()
+        if (rss_feed_queue):
+            new_feed = rss_feed_queue.pop()
+            print("Got a new feed: " + new_feed)
+            threading.Thread(target=pollRss, args=[new_feed]).start()
         else:
-            time.sleep(1)
-            print("Waiting for new html pages.")
+            time.sleep(60)
+            print("[{}] Waiting for new RSS feeds.".format(threading.current_thread().name))
 
-def parseHtml(html_response):
-    root = lxml.html.fromstring(html_response.body)
-    for link in root.xpath("//a"):
-        print(link.get("href"))
+
+def addOnlyNewRss(rss_feed, feed_data, entries):
+    for entry in entries:
+        concatenated_feed = entry.title + " " + entry.description
+        if not concatenated_feed in feed_data:
+            feed_data.add(concatenated_feed)
+            print("[{}] Adding: {}".format(rss_feed, concatenated_feed))
+
+def pollRss(rss_feed):
+    feed_data = set()
+
+    last_modified = None
+    while True:
+
+        feed = feedparser.parse(rss_feed, modified=last_modified)
+
+        if len(feed.entries) > 0:
+            last_modified = feed.modified
+            print("[{}] #### Adding new records. ####".format(rss_feed))
+            addOnlyNewRss(rss_feed, feed_data, feed.entries)
+        else:
+            print("[{}] Waiting for entries.".format(rss_feed))
+        time.sleep(10)
+
+
+
 
 
